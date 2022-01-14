93151f145573293a10f80594c2690e0e5cd88af8
Darcy Shen <da@tubi.tv>
2022-01-14 14:58:15 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import find_namespace_packages, setup
 
 setup(
     name="rikai-yolov5",
-    version="0.0.2",
+    version="0.0.3",
     license="Apache License, Version 2.0",
     author="Rikai authors",
     author_email="rikai-dev@eto.ai",
-    url="https://github.com/eto-ai/rikai",
+    url="https://github.com/da-tubi/rikai-yolov5",
     python_requires=">=3.7",
     install_requires=["rikai == 0.0.19", "yolov5 >=5.0.0, <6.0.0"],
     extras_require={
