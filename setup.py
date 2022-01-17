3de60ff9fc7aa35e555633275cd9e24b3d591836
Darcy Shen <da@tubi.tv>
2022-01-17 15:27:15 +0800

    bump to 0.0.4

--- setup.py
+++ setup.py
@@ -1,14 +1,22 @@
 from setuptools import find_namespace_packages, setup
+import pathlib
+
+with open(
+    pathlib.Path(__file__).absolute().parent / "README.md", "r"
+) as fh:
+    long_description = fh.read()
 
 setup(
     name="rikai-yolov5",
-    version="0.0.3",
+    version="0.0.4",
     license="Apache License, Version 2.0",
     author="Rikai authors",
     author_email="rikai-dev@eto.ai",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/da-tubi/rikai-yolov5",
     python_requires=">=3.7",
-    install_requires=["rikai >= 0.0.20", "yolov5 >=5.0.0, <6.0.0"],
+    install_requires=["rikai >= 0.0.20", "yolov5 >=6.0.0, <7.0.0"],
     extras_require={
         "dev": [
             "black",
